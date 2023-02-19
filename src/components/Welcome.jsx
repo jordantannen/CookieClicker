297b6c780de046eafdd7922e11c20322bf618478
Jordan Tannenbaum <jordantannen@gmail.com>
2023-02-19 14:23:31 -0500

    Add Sammy

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,12 @@
+import Sammy from "../img/sammy.jpeg"
+
 export default function Welcome() {
     return (
         <>
             <div className="wrapper">
                 <h1>Test</h1>
                 <p>Test part two</p>
+                <img src="Sammy" alt="Sammy" width={200} height={200}/>
             </div>
         </>
     )
