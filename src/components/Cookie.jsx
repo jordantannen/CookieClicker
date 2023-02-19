3396473ac90aa6028ce2260aa8cb821ac41f4969
Jordan Tannenbaum <jordantannen@gmail.com>
2023-02-19 15:18:25 -0500

    Add incremental click

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -7,14 +7,21 @@ export default function Cookie(){
     const [count, setCount] = useState(0)
 
     function increaseCount() {
-        setCount(prevCount => prevCount + 1)
+
+        let x = 1
+
+        if (count >= 10){
+            x = 2
+        }
+
+        setCount(prevCount => prevCount + x)
     }
 
     return(
         <div className="apple">
             <img className="cookie" onClick={increaseCount} src={CookieImg} alt="" width={200} height={200} />
             <h1>{count}</h1>
-            <div>{count > 5 ? (<div>test</div>) : <></>}</div>
+            
         </div>
     )
 }
