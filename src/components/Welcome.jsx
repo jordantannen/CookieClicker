a2c4c81dcfa2113b6bc741a3c8a735b5820d47e6
Jordan Tannenbaum <jordantannen@gmail.com>
2023-02-19 14:17:01 -0500

    Add welcome component

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+export default function Welcome() {
+    return (
+        <>
+            <div className="wrapper">
+                <h1>Test</h1>
+                <p>Test part two</p>
+            </div>
+        </>
+    )
+}
